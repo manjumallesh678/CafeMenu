556a115fed1ea8064fbd56b8646d82b4a6c67c2b
manjumallesh678 <56191254+manjumallesh678@users.noreply.github.com>
2020-05-25 22:35:46 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+for(var i = 0; i < names.length; i++){
+  if(names[i].charAt(0) == 'j' || names[i].charAt(0) == 'J'){
+    console.log("Good Bye " + names[i]);
+  }
+  else{
+    console.log("Hello " + names[i]);
+  }
+}
